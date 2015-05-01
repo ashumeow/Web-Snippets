2eb3866d96b6b163ce18c2583ef61effc7b2aa5c
Aswini S <ashumeow@live.com>
2015-05-02 04:19:01 +0530

    add log calculator

---
+++ log_calc.js
@@ -0,0 +1,26 @@
+//<script type="text/javascript">
+function logMeow2()
+{
+x = (document.getElementById("p1").value);
+y = Math.log(x)/Math.log(2);
+alert("log base 2 of "+x+" is "+y+"\n\n\251 2015 Aswini S");
+}
+//</script>
+
+//<script type="text/javascript">
+function Changebutton(x)
+{
+z = (document.getElementById("p4").value);
+document.getElementById(x).value = "Calculate log base "+z;
+}
+function logMeowBase()
+{
+z = (document.getElementById("p4").value);
+x = (document.getElementById("p2").value);
+y = Math.log(x)/Math.log(z);
+y = ((Math.round(y*1000000))/1000000);
+alert("log base "+z+" of "+x+" is "+y+"\n\n\251 2015 Aswini");
+}
+//</script>
+// Feeling so broken and sad... I'm stupid psycho without any sub-conscious.. That's what everybody calls me!
+// Wishing to die soon... or leave this place.. Why am i still alive? I don't deserve to live in this planet anymore :'(
